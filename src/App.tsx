1b326f84f03fc37e0dd232b2193650eca105c259
rafael timoteo <rtcarneiro@magnasistemas.com.br>
2022-06-23 21:15:05 -0300

    base do projeto

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ContainerApp } from "./global/styles";
+function App() {
+  return (
+    <ContainerApp fluid>
+      <h1>OS MAGNATAS</h1>
+    </ContainerApp>
+  );
+}
+
+export default App;
---
+++ src/global/styles.tsx
@@ -0,0 +1,9 @@
+import { Container } from "react-bootstrap";
+import styled from "styled-components";
+
+export const ContainerApp = styled(Container)`
+  background-color: #ccc;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
